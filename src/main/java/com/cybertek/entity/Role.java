cbf9de890d950bd0e17b89101342510d340f3416
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-24 19:38:00 -0600

    Ticketing project REST - Adding Aspect Oriented Layer

--- src/main/java/com/cybertek/entity/Role.java
+++ src/main/java/com/cybertek/entity/Role.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
