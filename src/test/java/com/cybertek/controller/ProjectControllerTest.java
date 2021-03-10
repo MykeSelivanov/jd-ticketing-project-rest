ff471e093fdc7f42907f3c02c1c8016fe65d5d2b
MykeSelivanov <michael777selivanov@gmail.com>
2021-03-09 18:54:44 -0600

    Ticketing project REST - Integration testing

--- src/test/java/com/cybertek/controller/ProjectControllerTest.java
+++ src/test/java/com/cybertek/controller/ProjectControllerTest.java
@@ -30,7 +30,7 @@ class ProjectControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJtaWtlIiwibGFzdE5hbWUiOiJzbWl0aCIsInN1YiI6Im1pY2hhZWw3NzdzZWxpdmFub3ZAZ21haWwuY29tIiwiaWQiOjIsInVzZXJOYW1lIjoibWljaGFlbDc3N3NlbGl2YW5vdkBnbWFpbC5jb20iLCJleHAiOjE2MTUxMDU0OTUsImlhdCI6MTYxNTA2OTQ5NX0.ENmsOfROM7r83GLkdkH2ahZ2d8SVUj6RpVbAXIF5DVo";
+    private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJtaWtlIiwibGFzdE5hbWUiOiJzbWl0aCIsInN1YiI6Im1pY2hhZWw3NzdzZWxpdmFub3ZAZ21haWwuY29tIiwiaWQiOjIsInVzZXJOYW1lIjoibWljaGFlbDc3N3NlbGl2YW5vdkBnbWFpbC5jb20iLCJleHAiOjE2MTUzNzEzMzQsImlhdCI6MTYxNTMzNTMzNH0.gXwY0brGIE0sf3Z6KP2tecnRRQrmI2-okyL8b020iW8";
 
     static UserDTO userDTO;
     static ProjectDTO projectDTO;
@@ -50,8 +50,8 @@ class ProjectControllerTest {
                 .build();
 
         projectDTO = projectDTO.builder()
-                .projectCode("API01")
-                .projectName("Api")
+                .projectCode("SPRING01")
+                .projectName("Test")
                 .assignedManager(userDTO)
                 .startDate(LocalDate.now())
                 .endDate(LocalDate.now().plusDays(5))
@@ -75,7 +75,7 @@ class ProjectControllerTest {
                 .header("Authorization",token)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].projectCode").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].projectCode").exists()) // $.data[0].projectCode jayway jsonPath
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].assignedManager.userName").isNotEmpty());
     }
 
@@ -84,10 +84,41 @@ class ProjectControllerTest {
        mockMvc.perform(MockMvcRequestBuilders
        .post("/api/v1/project")
        .header("Authorization",token)
+               .content(toJsonString(projectDTO))
        .contentType(MediaType.APPLICATION_JSON)
        .accept(MediaType.APPLICATION_JSON))
-       .andExpect(MockMvcResultMatchers.jsonPath("projectCode").isNotEmpty());
+       .andExpect(MockMvcResultMatchers.jsonPath("$.data.projectCode").isNotEmpty());
 
+    }
+
+    @Test
+    public void givenToken_updateProject() throws Exception {
+        // need to provide project id since we didn't provide it when built projectDTO in before class
+        projectDTO.setId(2L);
+
+        // set to test update
+        projectDTO.setProjectName("This is update Test");
+
+        mockMvc.perform(MockMvcRequestBuilders
+        .put("/api/v1/project")
+        .header("Authorization", token)
+        .content(toJsonString(projectDTO))
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("message").value("Project is updated"));
+
+    }
+
+    @Test
+    public void givenToken_deleteProject() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+        .delete("/api/v1/project/" + projectDTO.getProjectCode())
+        .header("Authorization", token)
+        .content(toJsonString(projectDTO))
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     protected String toJsonString(final Object obj){
@@ -100,5 +131,23 @@ class ProjectControllerTest {
             throw new RuntimeException();
         }
     }
+
+    // to get token dynamically with valid credentials
+//    private String getToken(String userName, String password) throws Exception {
+//        String json = mockMvc.perform(
+//                MockMvcRequestBuilders
+//                        .post("/authenticate")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .content("{" +
+//                                "\"username\": \"" + userName + "\"," +
+//                                "\"password\": \"" + password + "\"" +
+//                                "}")
+//        )
+//                .andExpect(status().isOk())
+//                .andReturn()
+//                .getResponse().getContentAsString();
+//        return jsonParser.parseMap(json).get("data").toString();
+//    }
 
 }
